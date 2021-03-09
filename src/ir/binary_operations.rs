4142bef5ebec6d01f0fa67e6f48ca9f0f3856f6a
Victor Omondi <victor@slick.co.ke>
2021-03-09 21:06:36 +0300

    Implement OpcodeStatement for BinaryExpression
    Signed-off-by: Victor Omondi <victor@slick.co.ke>

--- src/ir/binary_operations.rs
+++ src/ir/binary_operations.rs
@@ -1,4 +1,13 @@
-use super::Register;
+use std::convert::TryFrom;
+
+use nom::{combinator::map, number::complete::le_u8, sequence::tuple};
+
+use super::{Expression, Register, Statement};
+
+use crate::{
+    opcodes::Opcode,
+    parsers::{OpcodeStatement, ParserError, ParserResult},
+};
 
 #[derive(Debug)]
 pub enum BinaryOperator {
@@ -55,10 +64,61 @@ impl From<BinaryOperator> for &'static str {
     }
 }
 
+impl TryFrom<Opcode> for BinaryOperator {
+    type Error = ParserError;
+
+    fn try_from(opcode: Opcode) -> Result<Self, Self::Error> {
+        match opcode {
+            Opcode::Eq => Ok(Self::Equality),
+            Opcode::StrictEq => Ok(Self::Identity),
+            Opcode::Neq => Ok(Self::InEquality),
+            Opcode::StrictNeq => Ok(Self::NonIdentity),
+            Opcode::Less => Ok(Self::LessThan),
+            Opcode::LessEq => Ok(Self::LessThanEqual),
+            Opcode::Greater => Ok(Self::GreaterThan),
+            Opcode::GreaterEq => Ok(Self::GreaterThanEqual),
+            Opcode::Add | Opcode::AddN => Ok(Self::Addition),
+            Opcode::Mul | Opcode::MulN => Ok(Self::Multiplication),
+            Opcode::Div | Opcode::DivN => Ok(Self::Division),
+            Opcode::Mod => Ok(Self::Remainder),
+            Opcode::Sub | Opcode::SubN => Ok(Self::Subtraction),
+            Opcode::LShift => Ok(Self::LeftShift),
+            Opcode::RShift => Ok(Self::RightShift),
+            Opcode::BitAnd => Ok(Self::BitwiseAnd),
+            Opcode::BitXor => Ok(Self::BitwiseXor),
+            Opcode::InstanceOf => Ok(Self::InstanceOf),
+            Opcode::IsIn => Ok(Self::In),
+            _ => Err(ParserError::new(
+                "Opcode",
+                format!("{:?} is not a valid Binary operation", opcode),
+            )),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct BinaryExpression {
     operator: BinaryOperator,
     operands: (Register, Register),
+}
+
+impl OpcodeStatement for BinaryExpression {
+    fn parse(opcode: Opcode, input: &[u8]) -> ParserResult<Statement> {
+        let (remaining, result) = tuple((le_u8, le_u8, le_u8))(input)?;
+        let (register_byte, operand_1, operand_2) = result;
+
+        let expression = Expression::Binary(Self {
+            operator: BinaryOperator::try_from(opcode)?,
+            operands: (Register::from(operand_1), Register::from(operand_2)),
+        });
+
+        let statement = Statement::Expression {
+            register: Register::from(register_byte),
+            expression,
+        };
+
+        Ok((remaining, statement))
+    }
 }
 
 #[derive(Debug)]
