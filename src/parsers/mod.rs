44d9973bec6c5bfa3de52dd8b95895ed495a3f79
Victor Omondi <victor@slick.co.ke>
2021-03-09 17:10:02 +0300

    Define OpcodeStatement trait
    This trait will be used by IR types to parse opcodes into the
    various Statement variants
    
    Signed-off-by: Victor Omondi <victor@slick.co.ke>

--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -1,9 +1,16 @@
 use nom::IResult;
 
+use crate::{ir::Statement, opcodes::Opcode};
+
 mod bytecode;
 mod error;
+mod opcodes;
 
 pub use bytecode::bytecode_file_parser;
 pub use error::ParserError;
 
 pub type ParserResult<'a, O> = IResult<&'a [u8], O, ParserError>;
+
+pub trait OpcodeStatement: Sized {
+    fn parse(opcode: Opcode, input: &[u8]) -> ParserResult<Statement>;
+}
