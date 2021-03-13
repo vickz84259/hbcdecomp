057c6438f8f6b5a5d0024e0338782436667b80fa
Victor Omondi <victor@slick.co.ke>
2021-03-13 16:52:48 +0300

    Implement OpcodeStatement for Object
    Signed-off-by: Victor Omondi <victor@slick.co.ke>

--- src/ir/object/object_expression.rs
+++ src/ir/object/object_expression.rs
@@ -1,4 +1,10 @@
-use crate::ir::{Register, StringIndex};
+use nom::{combinator::map, number::complete::le_u8};
+
+use crate::{
+    ir::{Expression, Register, Statement, StringIndex},
+    opcodes::Opcode,
+    parsers::{OpcodeStatement, ParserError, ParserResult},
+};
 
 #[derive(Debug)]
 pub enum ArrayIndex {
@@ -35,6 +41,28 @@ pub enum Object {
     Global,
     This,
     Environment { id: u8 },
+}
+
+impl OpcodeStatement for Object {
+    fn parse(opcode: Opcode, input: &[u8]) -> ParserResult<Statement> {
+        let (input, register) = map(le_u8, Register::Byte)(input)?;
+
+        let (input, object) = match opcode {
+            Opcode::GetEnvironment => map(le_u8, |id| Object::Environment { id })(input),
+            Opcode::GetGlobalObject => Ok((input, Object::Global)),
+            Opcode::CreateEnvironment => Ok((input, Object::Environment { id: 0 })),
+            _ => Err(ParserError::new(
+                "Opcode",
+                format!("{:?} is not an Object", opcode),
+            ))?,
+        }?;
+
+        let statement = Statement::Expression {
+            register,
+            expression: Expression::Object(object),
+        };
+        Ok((input, statement))
+    }
 }
 
 #[derive(Debug)]
