4c2358354c69a2e7f28348738a11d82bebc6bc2a
Victor Omondi <victor@slick.co.ke>
2021-03-17 17:16:02 +0300

    Implement parser for new.target expression
    Signed-off-by: Victor Omondi <victor@slick.co.ke>

--- src/ir/expression.rs
+++ src/ir/expression.rs
@@ -1,6 +1,10 @@
+use nom::{combinator::map, number::complete::le_u8};
+
+use crate::parsers::ParserResult;
+
 use super::{
     BinaryExpression, CallExpression, EnvExpression, FrameCall, Literal, NewArrayExpression,
-    NewObjectExpression, Object, ObjectExpression, Register, UnaryExpression,
+    NewObjectExpression, Object, ObjectExpression, Register, Statement, UnaryExpression,
 };
 
 #[derive(Debug)]
@@ -16,4 +20,14 @@ pub enum Expression {
     Binary(BinaryExpression),
     FrameCall(FrameCall),
     CallExp(CallExpression),
+    NewTarget,
+}
+
+impl Expression {
+    pub fn parse_new_target(input: &[u8]) -> ParserResult<Statement> {
+        map(le_u8, |byte| Statement::Expression {
+            register: Register::Byte(byte),
+            expression: Self::NewTarget,
+        })(input)
+    }
 }
